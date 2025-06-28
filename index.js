220a899d5aa694834d760112cad158888e3e02fe
GlenJulius <maurusure@gmail.com>
2025-06-28 21:19:52 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Write your code here!// Remove the main#main element
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME
+
+// Append the new h1 to the body
+document.body.append(newHeader);
